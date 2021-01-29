25413589d5d17b0b9f24d1a08669aeb8ac9de5a0
y1z <whyis2cool@gmail.com>
2021-01-29 14:23:49 -0600

    Added static string to "MainMenuButtonActions"

--- GGJ2021T3/Assets/Scripts/MainMenuButtonActions.cs
+++ GGJ2021T3/Assets/Scripts/MainMenuButtonActions.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class MainMenuButtonActions : MonoBehaviour
 {
+
+  static public string Nivel1 = "Nivel1";
   public void quitGame()
   {
     Debug.Log("quitting the game");
     Application.Quit();
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
   }
 }
