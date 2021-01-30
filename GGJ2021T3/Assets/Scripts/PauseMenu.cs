1172c2e1be65ffdc254913dc46d61964ad04d2d6
sergiojdc <sergio.zoura@hotmail.com>
2021-01-29 17:15:20 -0700

    Correccion de suelos y de regresar al menu

--- GGJ2021T3/Assets/Scripts/PauseMenu.cs
+++ GGJ2021T3/Assets/Scripts/PauseMenu.cs
@@ -5,51 +5,56 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
 
-    public static bool GameIsPause = false;
+  public static bool GameIsPause = false;
 
-    public GameObject pauseMenuUI;
+  public GameObject pauseMenuUI;
 
-    // Update is called once per frame
-    void Update() {
-        
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            if(GameIsPause)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
-    }
+  // Update is called once per frame
+  void Update()
+  {
 
-    public void Resume() {
-       pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPause = false;
-        Debug.Log("Resume");
-    }
-
-    void Pause() {
-       pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        GameIsPause = true;
-        Debug.Log("Pause");
-
-    }
-
-
-    public void Exit()
+    if (Input.GetKeyDown(KeyCode.Escape))
     {
-        Debug.Log("Exit to menu");
-
+      if (GameIsPause)
+      {
+        Resume();
+      }
+      else
+      {
+        Pause();
+      }
     }
+  }
 
-    public void ExitGame()
-    {
-        Debug.Log("Quit game");
-        Application.Quit();
-    }
+  public void Resume()
+  {
+    pauseMenuUI.SetActive(false);
+    Time.timeScale = 1f;
+    GameIsPause = false;
+    Debug.Log("Resume");
+  }
+
+  void Pause()
+  {
+    pauseMenuUI.SetActive(true);
+    Time.timeScale = 0f;
+    GameIsPause = true;
+    Debug.Log("Pause");
+
+  }
+
+
+  public void Exit()
+  {
+    FindObjectOfType<Transiciones>().LoadScene("Intro");
+    Time.timeScale = 1f;
+    Debug.Log("Exit to menu");
+
+  }
+
+  public void ExitGame()
+  {
+    Debug.Log("Quit game");
+    Application.Quit();
+  }
 }
